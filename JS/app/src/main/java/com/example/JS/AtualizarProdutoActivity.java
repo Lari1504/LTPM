68a02cc0ad47008497ce930cd543cc7b59ca1c32
Lari1504 <128739011+Lari1504@users.noreply.github.com>
2024-06-10 12:05:42 -0300

    Add files via upload

---
+++ JS/app/src/main/java/com/example/JS/AtualizarProdutoActivity.java
@@ -0,0 +1,4 @@
+package com.example.JS;
+
+public class AtualizarProdutoActivity {
+}
---
+++ JS/app/src/main/java/com/example/JS/ProdutoActivity.java
@@ -0,0 +1,29 @@
+package com.example.JS;
+class Produto {
+    private final int codigo;
+    private final String nome;
+    private final double preco;
+    private final int quantidade;
+
+    public Produto(int codigo, String nome, double preco, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome(){
+        return nome;}
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+}
